d2dc298e31413ed602d6cf1df5870bc3f5532808
Nicole Tilbe <nicolecali2406@outlook.com>
2020-08-11 18:54:14 -0400

    working on insta clone still

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,11 +1,11 @@
+// You will add code to this file
 import React from 'react';
 import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
 
 const Post = props => {
-  // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { post, likePost } = props
 
   return (
     <div className='post-border'>
@@ -20,10 +20,8 @@ const Post = props => {
           src={post.imageUrl}
         />
       </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
+      <LikeSection id={post.id} likeCount={post.likes} likePost={() => likePost(post.id)} />
+      <Comments comments={props.post.comments} />
     </div>
   );
 };
