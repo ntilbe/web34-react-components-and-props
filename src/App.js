f238c82f465c056a95f73d790400f229b569023b
Nicole Tilbe <nicolecali2406@outlook.com>
2020-08-12 10:57:29 -0400

    finished stretch

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-/* 
-  Start here and work your way down the nested components.
-  Not all files in the project need code added.
-  Look at each file to see what props need to be passed!
-*/
-
-// Import the state hook
-import React from 'react';
-// Import the Posts (plural!) and SearchBar components, since they are used inside App component
-// Import the dummyData
-import './App.css';
+import React, { useState } from "react";
+import Posts from './components/Posts/Posts'
+import SearchBar from './components/SearchBar/SearchBar'
+import data from './dummy-data'
+import "./App.css";
 
 const App = () => {
-  // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
-  // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
+  const [posts, setPosts] = useState(data)
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [query, setQuery] = useState('')
+  const [filteredPosts, setFilteredPosts] = useState([])
+
+  const filter = e => {
+    e.preventDefault()
+    setQuery(e.target.value)
+    setFilteredPosts(posts.filter(post => post.username.toLowerCase().startsWith(e.target.value.toLowerCase())))
+  }
 
   const likePost = postId => {
-    /*
-      This function serves the purpose of increasing the number of likes by one, of the post with a given id.
-
-      The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
-      This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
-
-      Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
-      The callback passed into `map` performs the following logic:
-        - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
-        - otherwise just return the post object unchanged.
-     */
+    const updatedPosts = posts.map(post => {
+      if (post.id === postId) {
+        return {
+          ...post,
+          likes: post.likes + 1
+        }
+      }
+      return post
+    })
+    setPosts(updatedPosts)
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar query={query} filter={filter} />
+      <Posts likePost={likePost} posts={filteredPosts.length > 0 ? filteredPosts : posts} />
     </div>
   );
 };
